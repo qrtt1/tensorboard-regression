a926a252a4658f5a954a99a5b91b9ff920e2146c
Ching Yi, Chan <qrtt1@infuseai.io>
2020-12-02 17:06:26 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ check-tensorboard.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const baseUrl = process.argv[process.argv.length - 1];
+
+const wrongUrls = [];
+
+function checkResult(wrongUrls) {
+  if (wrongUrls.length == 0) {
+    console.log("All requests are good.")
+    process.exit(0);
+  } else {
+    console.log("Some requests are not good.")
+    process.exit(1);
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(10 * 1000);
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    console.log(request.url());
+    if (!request.url().startsWith(baseUrl)) {
+      console.log("URL without prefix: " + request.url());
+      if (!request.url().includes('/font-roboto/')) {
+        wrongUrls.push(request.url())
+      }
+    }
+    request.continue();
+  });
+
+  await page.goto(baseUrl);
+  await page.waitForNavigation({ waitUntil: 'load' }),
+  await browser.close();
+})().then(() => {
+  checkResult(wrongUrls);
+}).catch((e) => {
+  checkResult(wrongUrls);
+})
--- check-tensorboard.sh
+++ check-tensorboard.sh
@@ -1,23 +1,21 @@
-PORT=5566
-
 tensorboard --logdir tf-logs \
---port $PORT --bind_all \
+--port 5001 --bind_all \
 --path_prefix /foo/bar &
 
 echo "Tensorboard Starting"
 
-until printf "." && nc -z -w 2 127.0.0.1 $PORT; do
+until printf "." && nc -z -w 2 127.0.0.1 5001; do
     sleep 1;
 done;
 
 echo 'Tensorboard Started ✓'
 
-STATUS_CODE=$(curl -sL -w "%{http_code}" -I "http://127.0.0.1:$PORT/foo/bar/" -o /dev/null)
+STATUS_CODE=$(curl -sL -w "%{http_code}" -I "http://127.0.0.1:5001/foo/bar/" -o /dev/null)
 
 echo "STATUS_CODE is $STATUS_CODE"
-
-if [ $STATUS_CODE == "200" ]; then
-  exit 0
-else
+if [ $STATUS_CODE != "200" ]; then
+  echo "Failed to see the first page"
   exit 1
 fi
+
+node check-tensorboard.js http://127.0.0.1:5001/foo/bar/

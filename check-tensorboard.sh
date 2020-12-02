05f264f3b13b313d391a0a466206282cdd917857
Ching Yi, Chan <qrtt1@infuseai.io>
2020-12-02 16:03:38 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- check-tensorboard.sh
+++ check-tensorboard.sh
@@ -4,4 +4,12 @@ tensorboard --logdir tf-logs \
 
 echo "Tensorboard Started"
 
-ps aux
+STATUS_CODE=$(curl -sL -w "%{http_code}" -I "http://127.0.0.1/foo/bar" -o /dev/null)
+
+echo "STATUS_CODE is $STATUS_CODE"
+
+if [ $STATUS_CODE == "200" ]; then
+  exit 0
+else
+  exit 1
+fi
